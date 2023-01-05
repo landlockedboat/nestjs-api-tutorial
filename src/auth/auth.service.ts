5d478a3cb725473d7c1d7596c26eb065e7dff3f1
VICTOR ENRIQUE ALCAZAR LOPEZ <v.alcazarlopez@zurich.com>
2023-01-05 15:35:25 +0100

    introduce bookmarks to user object
    * make email field in user project unique
    * effect new prisma migration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,30 @@
-import { Injectable } from "@nestjs/common/decorators/core/injectable.decorator";
+import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dto';
+import * as argon from "argon2"
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
 
-    signup() {
+    constructor(private prisma: PrismaService) { }
 
-        return {msg: "signed up"}
+    async signup(dto: AuthDto) {
+        const hash = await argon.hash(dto.password);
 
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash,
+            }
+        })
+
+        delete user.hash; 
+
+        return user;
     }
 
     signin() {
-        return {msg: "signed in"}
+        return { msg: "signed in" }
     }
 }
 
