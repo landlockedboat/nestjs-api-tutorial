2840be31271148cebc83e1c9caed97f199ddd6bb
VICTOR ENRIQUE ALCAZAR LOPEZ <v.alcazarlopez@zurich.com>
2023-01-05 15:40:21 +0100

    add detailed error when email is duplicated on singon

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthDto } from './dto';
 import * as argon from "argon2"
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
@@ -11,16 +12,24 @@ export class AuthService {
     async signup(dto: AuthDto) {
         const hash = await argon.hash(dto.password);
 
-        const user = await this.prisma.user.create({
-            data: {
-                email: dto.email,
-                hash,
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                }
+            })
+            
+            delete user.hash;
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                // if error is duplicated field error
+                if (error.code === "P2002") {
+                    throw new ForbiddenException("Credentials taken")
+                }
             }
-        })
-
-        delete user.hash; 
-
-        return user;
+        }
     }
 
     signin() {
