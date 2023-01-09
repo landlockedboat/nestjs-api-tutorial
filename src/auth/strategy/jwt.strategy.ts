307eee7a8c1f892a40f1534beb845bc33dcafac7
VICTOR ENRIQUE ALCAZAR LOPEZ <v.alcazarlopez@zurich.com>
2023-01-09 12:53:01 +0100

    bootstrap e2e test
    * fix imports

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common/decorators";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
-import { PrismaService } from "src/prisma/prisma.service";
+import { PrismaService } from "../../prisma";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
