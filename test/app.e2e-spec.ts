eed75d8b358300a7352ab8e963b02343ca0a62f7
VICTOR ENRIQUE ALCAZAR LOPEZ <v.alcazarlopez@zurich.com>
2023-01-09 14:53:18 +0100

    add user edit endpoint + tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,6 +4,7 @@ import { PrismaService } from "../src/prisma/prisma.service";
 import { AppModule } from "../src/app.module";
 import * as pactum from "pactum";
 import { AuthDto } from "src/auth/dto";
+import { EditUserDto } from "src/user/dto";
 
 describe("App e2e", () => {
   let app: INestApplication;
@@ -98,7 +99,6 @@ describe("App e2e", () => {
 
 
 describe("User", () => {
-  
   describe("Get me", () => {
     it("should get current user", () => {
       return pactum
@@ -110,7 +110,25 @@ describe("User", () => {
         .expectStatus(200)
     })
   });
-  describe("Edit user", () => { });
+  describe("Edit user", () => {
+    it("should edit user", () => {
+      const dto : EditUserDto = {
+        firstName: "Blad",
+        email: "blad@tepes.com"
+      }
+      
+      return pactum
+        .spec()
+        .patch("/users")
+        .withHeaders({
+          Authorization: 'Bearer $S{userAt}'
+        })
+        .withBody(dto)
+        .expectStatus(200)
+        .expectBodyContains(dto.firstName)
+        .expectBodyContains(dto.email)
+    })
+  });
 });
 describe("Bookmarks", () => {
   describe("Create bookmark", () => { });
